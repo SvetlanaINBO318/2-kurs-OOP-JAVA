59364b4e1472c27e32232822a54fbb05e472d1f2
SvetlanaINBO318 <47588192+SvetlanaINBO318@users.noreply.github.com>
2019-10-01 23:08:03 +0300

    Create Main.java

---
+++ Practica /practic1 /Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+public static void main(String[] args) {
+
+Ball ball = new Ball(2, "розовый");
+
+System.out.println(ball.toString());
+
+Book book = new Book(740, "Одуванчики");
+
+System.out.println(book.toString());
+
+}
+
+}
